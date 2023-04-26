878b59cec33bb0935286aca1c60d0407d99e3ac8
José Luis Vilches <jose.luis.comvilches@gmail.com>
2023-04-26 00:23:50 -0300

    Agregamos model y servicio para proyecto

---
+++ src/app/model/proyecto.model.ts
@@ -0,0 +1,10 @@
+export class Proyecto{
+
+    id: number;
+    nombre_proyecto: string;
+    url_repositorio: string;
+    url_deploy: string;
+    descripcion_proyecto: string
+    imagen_proyecto: string;
+    
+}
---
+++ src/app/servicios/proyecto.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Proyecto } from '../model/proyecto.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProyectoService {
+  private URL = 'http://localhost:8080/skill/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public obtenerProyecto(id: number): Observable<Proyecto>{
+    return this.httpClient.get<Proyecto>(`${this.URL}${id}`);
+  }
+
+  public listaDeProyecto():Observable<Proyecto[]>{
+    return this.httpClient.get<Proyecto[]>(`${this.URL}proyectos`);
+  }
+
+  public crearProyecto(proyecto: Proyecto):Observable<Object>{
+    return this.httpClient.post(`${this.URL}crear`, proyecto);
+  }
+
+  public actualizarProyecto(id: number, proyecto: Proyecto): Observable<Object>{
+    return this.httpClient.put(`${this.URL}editar/${id}`, proyecto);
+  }
+
+  public eliminarProyecto(id: number): Observable<Object>{
+    return this.httpClient.delete(`${this.URL}eliminar/${id}`);
+  }
+
+}
