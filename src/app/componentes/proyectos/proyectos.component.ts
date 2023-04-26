1a2c67007cc6ff66035e3310330ea3934289647d
José Luis Vilches <jose.luis.comvilches@gmail.com>
2023-04-26 00:33:02 -0300

    Completamos el TS de proyecto con las funciones

--- src/app/componentes/proyectos/proyectos.component.ts
+++ src/app/componentes/proyectos/proyectos.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import Swal from 'sweetalert2';
 import { AutentificacionService } from 'src/app/servicios/autentificacion.service';
-import { PortfolioService } from '../../servicios/portfolio.service';
+import { Proyecto } from 'src/app/model/proyecto.model';
+import { ProyectoService } from 'src/app/servicios/proyecto.service';
+
 
 @Component({
   selector: 'app-proyectos',
@@ -11,23 +13,62 @@ import { PortfolioService } from '../../servicios/portfolio.service';
 export class ProyectosComponent {
 
   proyectoList: any;
-  constructor(private datosPortfolio: PortfolioService, private loginPrd: AutentificacionService){
+  id: number;
+  proyecto: Proyecto = new Proyecto();
 
-  }
+  constructor(private loginPrd: AutentificacionService, private proyectoService: ProyectoService){}
 
   ngOnInit(): void {
-    this.datosPortfolio.obtenerDatos().subscribe(data => {
-      this.proyectoList = data.projects;
-    });
+    
+    this.listProyecto(); 
+
   }
 
   public visualizarBotones():boolean{
     return this.loginPrd.hablitarLogueo()
   }
 
-  public eliminarProyecto(){
+  public obtenerIdProyecto(id: number) {
+    this.proyectoService.obtenerProyecto(id).subscribe(data => {
+      this.proyecto = data;
+    });
+  }
+
+  public limpiarProyecto(){
+    this.proyecto = new Proyecto();
+  }
+
+  public updateProyecto(id: number) {
+    this.proyectoService.actualizarProyecto(id, this.proyecto).subscribe(data => {
+      Swal.fire({
+        icon: 'info',
+        title: 'Proyecto Actualizado!!!',
+        showConfirmButton: false,
+        timer: 1800
+      })
+
+      this.listProyecto();
+
+    }, err => alert(err.message))
+  }
+
+  public addProyecto() {
+    this.proyectoService.crearProyecto(this.proyecto).subscribe(dato => {
+      Swal.fire({
+        icon: 'info',
+        title: 'Proyecto Agregado!!!',
+        showConfirmButton: false,
+        timer: 1800
+      })
+
+      this.listProyecto();
+
+    }, err => alert(err.message));
+  }
+
+  public deleteProyecto(id: number) {
     Swal.fire({
-      title: '¿Desea eliminar este proyecto?',
+      title: '¿Desea eliminar este Proyecto?',
       text: "Eliminar definitivamente",
       icon: 'warning',
       showCancelButton: true,
@@ -36,13 +77,29 @@ export class ProyectosComponent {
       confirmButtonText: 'Ok'
     }).then((result) => {
       if (result.isConfirmed) {
-        Swal.fire(
-          'Proyecto eliminado!!!',
-          'Tu proyecto fue eliminado',
-          'success'
-        )
+
+        this.proyectoService.eliminarProyecto(id).subscribe(data => {
+
+        });
+
+        Swal.fire({
+          icon: 'info',
+          title: 'Proyecto Eliminado !!!',
+          showConfirmButton: false,
+          timer: 1800
+        })
+
+        this.listProyecto();
+
       }
     })
+
+  }
+
+  private listProyecto() {
+    this.proyectoService.listaDeProyecto().subscribe(data => {
+      this.proyectoList = data;
+    });
   }
 
 }
