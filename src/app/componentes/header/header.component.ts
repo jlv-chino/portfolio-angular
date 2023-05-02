03083b7da939a2cca5ab52da28c5e974ea34a060
José Luis Vilches <jose.luis.comvilches@gmail.com>
2023-05-02 03:28:17 -0300

    Función Logout completa

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -27,14 +27,15 @@ export class HeaderComponent {
   public inputs: boolean = true;
   buttonText: string = 'Login';
 
-  constructor(private tokenService: TokenService, private authService: AuthService,
-              private router: Router) { }
+  constructor(private tokenService: TokenService, private authService: AuthService) { }
 
   ngOnInit(): void {
     if (this.tokenService.getToken()) {
       this.isLogged = true;
       this.isLogginFail = false;
       this.roles = this.tokenService.getAuthorities();
+      this.inputs = false;
+      this.buttonText = 'Logout';
     }
   }
 
@@ -49,54 +50,54 @@ export class HeaderComponent {
       Swal.fire({
         icon: 'info',
         title: 'Sesión cerrada con éxito',
-        //showConfirmButton: false,
-        //timer: 1800
-      })
-
-      /*setTimeout(function () {
-        window.location.reload();
-      }, 1800);*/
-
-      return;
-    }
-
-    this.loginUsuario = new LoginUsuario(this.nombreUsuario, this.password);
-    this.authService.login(this.loginUsuario).subscribe(data => {
-      this.isLogged = true;
-      this.isLogginFail = false;
-      this.tokenService.setToken(data.token);
-      this.tokenService.setUserName(data.nombreUsuario);
-      this.tokenService.setAuthorities(data.authorities);
-      this.roles = data.authorities;
-
-      this.inputs = false;
-      this.buttonText = 'Logout';
-
-      this.router.navigate([''])
-
-      Swal.fire({
-        icon: 'success',
-        title: 'Login',
-        text: 'Usuario correcto!!!',
-        //showConfirmButton: false,
-        //timer: 1800
+        showConfirmButton: false,
+        timer: 1800
       })
 
       setTimeout(function () {
-      window.location.reload();
+        window.location.reload();
       }, 1800);
 
-    }, err => {
-      this.isLogged = false;
-      this.isLogginFail = true;
-      this.errMsj = err.error.mensaje;
-      console.log(this.errMsj);
-      Swal.fire({
-        icon: 'error',
-        title: 'Error',
-        text: 'Usuario o password invalido'
+    } else {
+
+      this.loginUsuario = new LoginUsuario(this.nombreUsuario, this.password);
+      this.authService.login(this.loginUsuario).subscribe(data => {
+        this.isLogged = true;
+        this.isLogginFail = false;
+        this.tokenService.setToken(data.token);
+        this.tokenService.setUserName(data.nombreUsuario);
+        this.tokenService.setAuthorities(data.authorities);
+        this.roles = data.authorities;
+
+        this.inputs = false;
+        this.buttonText = 'Logout';
+
+        Swal.fire({
+          icon: 'success',
+          title: 'Login',
+          text: 'Usuario correcto!!!',
+          showConfirmButton: false,
+          timer: 1800
+        })
+
+        setTimeout(function () {
+          window.location.reload();
+        }, 1800);
+
+      }, err => {
+        this.isLogged = false;
+        this.isLogginFail = true;
+        this.errMsj = err.error.mensaje;
+        console.log(this.errMsj);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Usuario o password invalido'
+        })
       })
-    })
+
+    }
+
   }
 
 
